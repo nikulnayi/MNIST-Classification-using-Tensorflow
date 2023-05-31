c4c533a09c9e6b131fa228f1b45fc5397fffc041
Nikul Nayi <nayinikul97@gmail.com>
2023-05-31 16:56:45 -0400

    Made to code Reusable

--- mnist_example.py
+++ mnist_example.py
@@ -2,6 +2,10 @@ import tensorflow
 import matplotlib.pyplot as plt
 import numpy as np
 from tensorflow.keras.layers import Conv2D, Input, Dense,  MaxPool2D, BatchNormalization, GlobalAvgPool2D
+from deeplearningModels import functional_model,MyCustomeModel
+from utils import display_some_example
+
+
 #Tensorflow.Keras.Model
 seq_model = tensorflow.keras.Sequential(
     [
@@ -20,71 +24,6 @@ seq_model = tensorflow.keras.Sequential(
         Dense(10,activation='softmax')
     ]
 )   
-
-#Functinal Approch 
-def functional_model():
-    myInput = Input(shape=(28, 28, 1))
-
-    x = Conv2D(32,(3,3), activation='relu')(myInput)
-    x = Conv2D(64,(3,3), activation='relu')(x)
-    x = MaxPool2D()(x)
-    x = BatchNormalization()(x)
-
-    x = Conv2D(128,(3,3), activation='relu')(x)
-    x = MaxPool2D()(x)
-    x = BatchNormalization()(x)
-
-    x = GlobalAvgPool2D()(x)
-    x = Dense(64, activation='relu')(x)
-    x = Dense(10,activation='softmax')(x)
-
-    model = tensorflow.keras.Model(inputs=myInput,outputs = x)
-    return model
-
-#Tensorflow.Keras.Model : Inherit from the class
-class MyCustomeModel(tensorflow.keras.Model):
-    def __init__(self):
-        super().__init__()    
-        self.conv1 = Conv2D(32,(3,3), activation='relu')
-        self.conv2 = Conv2D(64,(3,3), activation='relu')
-        self.maxpool1 = MaxPool2D()
-        self.batchnorm1 = BatchNormalization()
-        self.conv3 = Conv2D(128,(3,3), activation='relu')
-        self.maxpool2 = MaxPool2D()
-        self.batchnorm2 = BatchNormalization()
-        self.globalavgpool1 = GlobalAvgPool2D()
-        self.dense1 = Dense(64, activation='relu')
-        self.dense2 = Dense(10,activation='softmax')
-
-    def call(self, MyInput):
-
-        x = self.conv1(MyInput)
-        x = self.conv2(x)
-        x = self.maxpool1(x)
-        x = self.batchnorm1(x)
-
-        x = self.conv3(x)
-        x = self.maxpool2(x)
-        x = self.batchnorm2(x)
-
-        x = self.globalavgpool1(x)
-        x = self.dense1(x)
-        x = self.dense2(x)
-
-        return x
-
-def display_some_example(examples, labels):
-
-    plt.figure(figsize=(10,10))
-    for i in range(25):
-        idx = np.random.randint(0 , examples.shape[0]-1)
-        img = examples[idx]
-        label = labels[idx]
-        plt.subplot(5,5,i+1)
-        plt.title(str(label))
-        plt.tight_layout()
-        plt.imshow(img, cmap='gray')
-    plt.show()
 
 if __name__=='__main__':
     (x_train, y_train), (x_test, y_test) = tensorflow.keras.datasets.mnist.load_data()
